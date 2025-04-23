2abc97aa3099cecce9bbae94bfe8adf606d7f458
ABDULAHAD118 <129290085+ABDULAHAD118@users.noreply.github.com>
2025-04-23 14:04:57 +0500

    Add IncomeOverview component and implement income data fetching in Income page

---
+++ Frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+import { RxCross2 } from 'react-icons/rx';
+interface ModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title: string;
+    children: React.ReactNode;
+}
+
+const Modal = (props: ModalProps) => {
+    const { isOpen, onClose, title, children } = props;
+    if (!isOpen) return null;
+    return (
+        <div className="bg-opacity-50 fixed top-0 right-0 left-0 z-50 flex h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-x-hidden overflow-y-auto bg-black/50">
+            <div className="relative max-h-full w-full max-w-2xl p-4">
+                <div className="rounded-lg bg-white shadow-sm dark:bg-gray-700">
+                    <div className="flex items-center justify-between rounded-t border-b border-gray-200 p-4 md:p-5 dark:border-gray-600">
+                        <h3 className="text-lg font-medium text-gray-900 dark:text-white">
+                            {title}
+                        </h3>
+                        <button
+                            type="button"
+                            className="inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+                            onClick={onClose}
+                        >
+                            <RxCross2 className="h-5 w-5" />
+                        </button>
+                    </div>
+                    <div className="space-y-4 p-4 md:p-5">{children}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
--- Frontend/src/pages/Dashboard/Income.tsx
+++ Frontend/src/pages/Dashboard/Income.tsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from 'react';
 import DashboardLayout from '../../components/Layouts/DashboardLayout';
+import IncomeOverview from '../../components/Income/IncomeOverview/IncomeOverview';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATH } from '../../utils/apiPath';
+import Modal from '../../components/Modal/Modal';
 
 const Income = () => {
+    const [incomeData, setIncomeData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [openDeleteAlert, setOpenDeleteAlert] = useState({
+        show: false,
+        data: null,
+    });
+    const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+
+    const fetchIncomeDetails = async () => {
+        if (loading) return;
+        setLoading(true);
+        try {
+            const response = await axiosInstance.get(
+                API_PATH.INCOME.GET_ALL_INCOME
+            );
+            if (response.data) {
+                setIncomeData(response.data);
+            }
+        } catch (error) {
+            console.error('Error fetching income details:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    const handleAddIncome = async (income: string) => {};
+    const handleDeleteIncome = async (id: string) => {};
+    const handlePDFDownloadIncomeDetails = async () => {};
+    const handleExcelDownloadIncomeDetails = async () => {};
+
+    useEffect(() => {
+        fetchIncomeDetails();
+    }, []);
+
     return (
         <DashboardLayout activeMenu="Income">
-            <div>Income</div>
+            <div className="mx-auto my-5">
+                <div className="grid grid-cols-1 gap-6">
+                    <div className="">
+                        <IncomeOverview
+                            transactions={incomeData}
+                            onAddIncome={() => setOpenAddIncomeModal(true)}
+                        />
+                    </div>
+                </div>
+                <Modal
+                    isOpen={openAddIncomeModal}
+                    onClose={() => setOpenAddIncomeModal(false)}
+                    title="Add Income"
+                >
+                    <div>Add Income Form</div>
+                </Modal>
+            </div>
         </DashboardLayout>
     );
 };
